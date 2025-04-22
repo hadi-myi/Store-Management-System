100a8382ad094d100179d10aa3862e77378088e0
poultong <113626402+poultong@users.noreply.github.com>
2025-04-22 18:55:42 -0500

    Finished project_part2.py

--- project_part2.py
+++ project_part2.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 from mysql.connector import errorcode
 
-def shipment(store : int, delivery_date : str, reorders : list [int], shipment_items : dict, shipment_no : int, cnx : mysql.connector.connection):
+def shipment(store : int, delivery_date : str, reorders : list [int], shipment_items : dict, shipment_no : int, vendor : int, cnx : mysql.connector.connection):
 
  # check if connection is open
     if cnx is None or not cnx.is_connected():
@@ -25,7 +25,7 @@ def shipment(store : int, delivery_date : str, reorders : list [int], shipment_i
            # first_query = "SELECT REORDER_REQUEST.UPC, REORDER_REQUEST.quantity_requested, REORDER_REQUEST.reorder_id FROM REORDER_REQUEST WHERE REORDER_REQUEST.store_id = {} AND REORDER_REQUEST.reorder_id IN ({});".format(store, reorder_string)
             #cursor.execute(first_query)
 
-            cursor.execute("SELECT REORDER_REQUEST.UPC, REORDER_REQUEST.quantity_requested, REORDER_REQUEST.reorder_id FROM REORDER_REQUEST WHERE REORDER_REQUEST.store_id = %s", ([store]))
+            cursor.execute("SELECT REORDER_REQUEST.UPC, REORDER_REQUEST.quantity_requested, REORDER_REQUEST.reorder_id FROM REORDER_REQUEST WHERE REORDER_REQUEST.store_id = %s AND REORDER_REQUEST.vendor_id = %s", (store, vendor))
             products = cursor.fetchall()
 
             fufilled_orders = []
@@ -40,7 +40,6 @@ def shipment(store : int, delivery_date : str, reorders : list [int], shipment_i
                     if cur_quantity >= x[1]:
                         # update dictionary to new current quantity of that item x[1] - cur_quantity
                         shipment_items[x[0]] = x[1] - cur_quantity
-                        print(x[0], x[1], x[2])
                         fufilled_orders.append((x[0], x[1], x[2]))
 
 
@@ -58,13 +57,21 @@ def shipment(store : int, delivery_date : str, reorders : list [int], shipment_i
                 cursor.execute("INSERT INTO SHIPMENT (shipment_no, reorder_id, expected_delivery) VALUES (%s, %s, %s);", (shipment_no, x[2], delivery_date))
                 cnx.commit()
 
-            print("\n")
             print("Store #" + str(store) + " Manifest:")
-            print(" QUANTITY | ITEM")
+            print("Expected Date and Time of Delievery: " + delivery_date)
+            print("QUANTITY | ITEM")
+         
             for x in fufilled_orders:
+                print(x[0])
                 cursor.execute("SELECT PRODUCT.product_name FROM PRODUCT WHERE PRODUCT.UPC = %s", ([x[0]]))
                 item = cursor.fetchall()
-                print(x[1], ": " + item[0][0])
+                print(" ", x[1], ": " + item[0][0])
+
+            print("\nThere is currently:" + str((len(products) - len(fufilled_orders))) + " order(s) left for store #", store)
+            cursor.execute("SELECT REORDER_REQUEST.UPC, REORDER_REQUEST.quantity_requested, REORDER_REQUEST.reorder_id FROM REORDER_REQUEST WHERE REORDER_REQUEST.vendor_id = %s",  ([vendor]))
+            selected_vendor = cursor.fetchall()
+            print("There is currently:" + str((len(selected_vendor) - len(fufilled_orders))) + " BMART order(s) left for vendor #", vendor)
+
             
 
     except  mysql.connector.Error as err:
